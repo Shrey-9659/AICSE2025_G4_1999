232be7ffeaee237661e957deac4aa07c5a86016c
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2025-11-19 12:05:12 +0530

    JS update

---
+++ JS/G7P/DOM/script.js
@@ -0,0 +1,25 @@
+// console.log("This JS file is running...")
+
+// 1. Selecting an element
+
+// getElementById
+// console.log(document.getElementById("heading"))
+// let h1 = document.getElementById("heading")
+// console.log(h1)
+
+// getElementByTagName
+// console.log(document.getElementsByTagName("h1"))
+
+// getElementByClassName
+// console.log(document.getElementsByClassName("grouping"))
+
+// querySelector
+// console.log(document.querySelector(".grouping"))
+// console.log(document.querySelector("h1"))
+
+// querySelectorAll
+// console.log(document.querySelectorAll("h1"))
+
+// Getting the value of an element
+// Setting the value of an element
+// Event Listeners
